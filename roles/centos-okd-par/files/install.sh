a178dfb6d6dfafc0e04f40cd09146669380dcffe
fabstao <fabian@nuo.com.mx>
2020-01-22 20:37:45 -0600

    Cleaning

--- roles/centos-okd-par/files/install.sh
+++
@@ -1,8 +0,0 @@
-export https_proxy=http://proxy-dmz.intel.com:911 
-export http_proxy=http://proxy-dmz.intel.com:911 
-export PROXY=proxy-dmz.intel.com 
-export PORT=911
-export PROXY_TYPE=http-connect
-export PAC_URL=http://wpad.intel.com 
-export EXCEPTIONS=/root/chameleonsocks.exceptions
-/root/chameleonsocks.sh --install
